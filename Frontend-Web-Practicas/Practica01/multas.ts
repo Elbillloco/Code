d04e04fccb5ca7dd30414b7655dd2164c471305e
Elbillloco <roamarillas3@gmail.com>
2026-09-02 09:12:04 -0700

    Ejercicio1_Frontend-Web-Practicas

---
+++ Frontend-Web-Practicas/Practica01/multas.ts
@@ -0,0 +1,59 @@
+interface Prestamo{
+    multa: number;
+    ejemplar: number;
+}
+
+function calcularMulta(prestamo : Prestamo): number{
+    const cargoFijo = 50;
+    return prestamo.multa + cargoFijo;
+}
+
+const prestamo: Prestamo = {multa: 350, ejemplar: 14};
+prestamo.multa = 350;
+
+/*Al correr el codigo, imprime en consola 35014 por que
+Detecta las comillas simples de multa como un String 
+Lo traduce como un Int*/
+console.log(calcularMulta(prestamo));
+
+/*Responder: Hubo algun error, alguna advertencia o algo en la consola que avisara?
+Respuesta: Si, como tal el error esta en la impresion en consola, no hace
+La operacion matematica correctamente, sino que lo concatena como un string*/
+
+/*Responder: Si el archivo tiene un error de tipos, por que node lo ejecuta?
+Cual comando revisa y cual ejecuta?
+Respuesta : Node ejecutara el codigo independientemente si hay un error, correra
+Hasta donde el codigo esta correctamente*/
+
+const valor_numerico_1: number = 7;
+/*valor_numerico_1 = 1; Esto no puede suceder debido a que const es solo para valores
+que no cambiaran o que seran fijos*/
+
+interface cuenta_banco{
+    id : number;
+    nombre_usuario: string;
+    saldo : number;
+}
+
+
+const cuenta_banco: cuenta_banco = {
+    id: 1, 
+    nombre_usuario: "Bayonetta",
+    saldo: 350
+};
+cuenta_banco.saldo = 0;
+
+let mensaje_banco;
+mensaje_banco = "Saldo actual de: " + cuenta_banco.nombre_usuario + ' es de ' + '$'+ cuenta_banco.saldo;
+console.log(mensaje_banco);
+
+/*Responder: de las dos lineas que usan const, Por que solo una falla?
+Respuesta: esta falla por que const no puede modificarse una vez declarado, esto debido a
+que es una constante, la naturaleza y lo que no permite TS es la modificacion de este.
+
+Responder: al asignarle un texto a la variable let, nadie escribio que fuera un numero, De donde salio ese tipo?
+Respuesta: al ser una variable Let, este tipo cambia dependiendo del valor que se le asigne, ejemplo, si el valor tiene
+comillas, se hara un string, si es numerico simple, se hara Integer, mientras que si se un true o false, sera booleano.*/
+
+type Estado_objeto = 'activo' | 'devuelto' | 'vencido';
+let Estado_objeto = 'ACTIVO';
