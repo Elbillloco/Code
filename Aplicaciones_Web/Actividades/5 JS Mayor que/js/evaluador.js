10f7b1f9444703cf1ca67c4d07379f239215390f
Elbillloco <roamarillas3@gmail.com>
2026-09-14 07:16:32 -0700

    a

--- Aplicaciones_Web/Actividades/5 JS Mayor que/js/evaluador.js
+++ Aplicaciones_Web/Actividades/5 JS Mayor que/js/evaluador.js
@@ -2,10 +2,16 @@ const btnComparar = document.getElementById("btnComparar");
 
 btnComparar.addEventListener("click", function(){
     
-    const number01 = document.getElementById("number01").value;
-    const number02 = document.getElementById("number02").value;
+    const number01 = Number(document.getElementById("number01").value);
+    const number02 = Number(document.getElementById("number02").value);
 
     const resultado = document.getElementById("resultado");
+    const contenedorResultado = document.querySelector(".color_resultado");
+
+    if (isNaN(number01) || isNaN(number02)){
+        resultado.textContent = "Debe ingresar solamente numeros";
+        return;
+    }
 
     if (number01 > number02){
         resultado.textContent = ("El numero " + number01 + " es el numero mayor");
@@ -17,8 +23,11 @@ btnComparar.addEventListener("click", function(){
         resultado.textContent = ("Los numeros son iguales");
     }
 
-    if (isNaN(number01) || isNaN(number02)){
-        resultado.textContent = "Debe ingresar solamente numeros";
-        return;
+    const numeroMayor = Math.max(number01, number02);
+
+    if (numeroMayor > 100){
+        contenedorResultado.style.backgroundColor = "rgb(75, 4, 74)";
+    } else {
+        contenedorResultado.style.backgroundColor = "rgb(87, 10, 20)";
     }
 })
